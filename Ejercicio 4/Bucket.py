194764bd608e6bb584cb9d77d4d9cc7928fe0685
Sakarrex <sebasgarciafontana@gmail.com>
2022-10-21 18:55:54 -0300

    A

--- Ejercicio 4/Bucket.py
+++ Ejercicio 4/Bucket.py
@@ -5,15 +5,30 @@ class Bucket:
     __contador = None
 
     def __init__(self,tamanio) -> None:
-        self.__arreglo = np.empty(tamanio,dtype= int)
+        self.__arreglo = np.zeros(tamanio,dtype= int)
         self.__contador = 0
     
-    def agregar(self,elem,pos):
-        self.__arreglo[pos] = elem
-        self.__contador += 1
+    def agregar(self,elem):
+        resultado = False
+        if elem in self.__arreglo:
+            print("Clave repetida")
+        elif self.__contador < len(self.__arreglo):
+            self.__arreglo[self.__contador] = elem
+            self.__contador += 1
+            resultado = True
+        return resultado
+    
+    def isElement(self,elem):
+        return elem in self.__arreglo
     
     def getElem(self,pos):
         return self.__arreglo[pos]
     
+    def estaLleno(self):
+        return self.__contador == len(self.__arreglo)
+
     def getCant(self):
         return self.__contador
+
+    def __str__(self) -> str:
+        return str(self.__arreglo)
